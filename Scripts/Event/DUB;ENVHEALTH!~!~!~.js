7d9b155206d86aaed6d391ea48863870ae0bd0ba
YashSepTech <58787347+YashSepTech@users.noreply.github.com>
2020-07-07 18:34:35 +0530

    Added condition to only run in ACA

--- Scripts/Event/DUB;ENVHEALTH!~!~!~.js
+++ Scripts/Event/DUB;ENVHEALTH!~!~!~.js
@@ -1,16 +1,19 @@
-try{
-	var inCapScriptModel = aa.cap.getCap(capId).getOutput();
-	if(inCapScriptModel){
-		var tempCapModel = inCapScriptModel.getCapModel();
-		if(tempCapModel.getCapClass()=="COMPLETE"){
-			if (capStatus != "Additional Info Required"){
-				cancel = true;
-				showMessage = true;
-				comment("Record must have the status 'Additional Info Required' before documents can be uploaded.");
+if(publicUser)
+{
+	try{
+		var inCapScriptModel = aa.cap.getCap(capId).getOutput();
+		if(inCapScriptModel){
+			var tempCapModel = inCapScriptModel.getCapModel();
+			if(tempCapModel.getCapClass()=="COMPLETE"){
+				if (capStatus != "Additional Info Required"){
+					cancel = true;
+					showMessage = true;
+					comment("Record must have the status 'Additional Info Required' before documents can be uploaded.");
+				}
 			}
 		}
+	}catch(err){
+	  logDebug("A JavaScript Error occurred: DUB:EnvHealth/~/~/~: " + err.message);
+	  logDebug(err.stack)
 	}
-}catch(err){
-  logDebug("A JavaScript Error occurred: DUB:EnvHealth/~/~/~: " + err.message);
-  logDebug(err.stack)
 }
