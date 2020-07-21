3e92a43ff145617975a542106551ca39587c342b
kirstenCG <63075392+kirstenCG@users.noreply.github.com>
2020-07-21 11:35:48 -0600

    addfee function

--- INCLUDES_CUSTOM/INCLUDES_SEP_FUNCTIONS.js
+++ INCLUDES_CUSTOM/INCLUDES_SEP_FUNCTIONS.js
@@ -1,67 +1,4 @@
 //INCLUDES_SEP_CUSTOM START
-//start: corrected standard functions
-function doScriptActions() {
-	if (typeof(appTypeArray) == "object") {
-		include(prefix + ":*/*/*/*");
-		include(prefix + ":" + appTypeArray[0] + "/*/*/*");
-		include(prefix + ":" + appTypeArray[0] + "/" + appTypeArray[1] + "/*/*");
-		include(prefix + ":" + appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/*");
-		include(prefix + ":" + appTypeArray[0] + "/*/" + appTypeArray[2] + "/*");
-		include(prefix + ":" + appTypeArray[0] + "/*/" + appTypeArray[2] + "/" + appTypeArray[3]);
-		include(prefix + ":" + appTypeArray[0] + "/*/*/" + appTypeArray[3]);
-		include(prefix + ":" + appTypeArray[0] + "/" + appTypeArray[1] + "/*/" + appTypeArray[3]);
-		include(prefix + ":" + appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/" + appTypeArray[3]);
-	}
-}
-
-function addFee(fcode, fsched, fperiod, fqty, finvoice){
-try{
-	// Updated Script will return feeSeq number or null if error encountered (SR5112)
-	var feeCap = capId;
-	var feeCapMessage = "";
-	var feeSeq_L = new Array(); // invoicing fee for CAP in args
-	var paymentPeriod_L = new Array(); // invoicing pay periods for CAP in args
-	var feeSeq = null;
-	if (arguments.length > 5) {
-		feeCap = arguments[5]; // use cap ID specified in args
-		feeCapMessage = " to specified CAP";
-	}
-	assessFeeResult = aa.finance.createFeeItem(feeCap, fsched, fcode, fperiod, fqty);
-	if (assessFeeResult.getSuccess()) {
-		feeSeq = assessFeeResult.getOutput();
-		logMessage("Successfully added Fee " + fcode + ", Qty " + fqty + feeCapMessage);
-		logDebug("The assessed fee Sequence Number " + feeSeq + feeCapMessage);
-		if (finvoice == "Y" && arguments.length == 5){
-			feeSeqList.push(feeSeq);
-			paymentPeriodList.push(fperiod);
-			var invoiceResult_L = aa.finance.createInvoice(feeCap, feeSeqList, paymentPeriodList);
-			if (invoiceResult_L.getSuccess()){
-				logMessage("Invoicing assessed fee items" + feeCapMessage + " is successful.");
-			}else{
-				logDebug("**ERROR: Invoicing the fee items assessed" + feeCapMessage + " was not successful.  Reason: " + invoiceResult.getErrorMessage());
-			}
-		}
-		if (finvoice == "Y" && arguments.length > 5) {
-			feeSeq_L.push(feeSeq);
-			paymentPeriod_L.push(fperiod);
-			var invoiceResult_L = aa.finance.createInvoice(feeCap, feeSeq_L, paymentPeriod_L);
-			if (invoiceResult_L.getSuccess())
-				logMessage("Invoicing assessed fee items" + feeCapMessage + " is successful.");
-			else
-				logDebug("**ERROR: Invoicing the fee items assessed" + feeCapMessage + " was not successful.  Reason: " + invoiceResult.getErrorMessage());
-		}
-		updateFeeItemInvoiceFlag(feeSeq, finvoice);
-	}else {
-		logDebug("**ERROR: assessing fee (" + fcode + "): " + assessFeeResult.getErrorMessage());
-		feeSeq = null;
-	}
-	return feeSeq;
-}catch(err){
-	logDebug("An error occurred in addFee: " + err.message);
-	logDebug(err.stack);
-}}
-//end corrected standard functions 
-
 
 function sepGetReqdDocs() {
 try{
@@ -2036,5 +1973,68 @@ try{
 	logDebug(err.stack)
 }}
 
+//start: corrected standard functions
+function doScriptActions() {
+	if (typeof(appTypeArray) == "object") {
+		include(prefix + ":*/*/*/*");
+		include(prefix + ":" + appTypeArray[0] + "/*/*/*");
+		include(prefix + ":" + appTypeArray[0] + "/" + appTypeArray[1] + "/*/*");
+		include(prefix + ":" + appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/*");
+		include(prefix + ":" + appTypeArray[0] + "/*/" + appTypeArray[2] + "/*");
+		include(prefix + ":" + appTypeArray[0] + "/*/" + appTypeArray[2] + "/" + appTypeArray[3]);
+		include(prefix + ":" + appTypeArray[0] + "/*/*/" + appTypeArray[3]);
+		include(prefix + ":" + appTypeArray[0] + "/" + appTypeArray[1] + "/*/" + appTypeArray[3]);
+		include(prefix + ":" + appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/" + appTypeArray[3]);
+	}
+}
+
+function addFee(fcode, fsched, fperiod, fqty, finvoice){
+try{
+	// Updated Script will return feeSeq number or null if error encountered (SR5112)
+	var feeCap = capId;
+	var feeCapMessage = "";
+	var feeSeq_L = new Array(); // invoicing fee for CAP in args
+	var paymentPeriod_L = new Array(); // invoicing pay periods for CAP in args
+	var feeSeq = null;
+	if (arguments.length > 5) {
+		feeCap = arguments[5]; // use cap ID specified in args
+		feeCapMessage = " to specified CAP";
+	}
+	assessFeeResult = aa.finance.createFeeItem(feeCap, fsched, fcode, fperiod, fqty);
+	if (assessFeeResult.getSuccess()) {
+		feeSeq = assessFeeResult.getOutput();
+		logDebug("Successfully added Fee " + fcode + ", Qty " + fqty + feeCapMessage);
+		logDebug("The assessed fee Sequence Number " + feeSeq + feeCapMessage);
+		if (finvoice == "Y" && arguments.length == 5){
+			logDebug("here");
+			feeSeqList.push(feeSeq);
+			paymentPeriodList.push(fperiod);
+			var invoiceResult_L = aa.finance.createInvoice(feeCap, feeSeqList, paymentPeriodList);
+			if (invoiceResult_L.getSuccess()){
+				logDebug("Invoicing assessed fee items" + feeCapMessage + " is successful.");
+			}else{
+				logDebug("**ERROR: Invoicing the fee items assessed" + feeCapMessage + " was not successful.  Reason: " + invoiceResult.getErrorMessage());
+			}
+		}
+		if (finvoice == "Y" && arguments.length > 5) {
+			feeSeq_L.push(feeSeq);
+			paymentPeriod_L.push(fperiod);
+			var invoiceResult_L = aa.finance.createInvoice(feeCap, feeSeq_L, paymentPeriod_L);
+			if (invoiceResult_L.getSuccess())
+				logDebug("Invoicing assessed fee items" + feeCapMessage + " is successful.");
+			else
+				logDebug("**ERROR: Invoicing the fee items assessed" + feeCapMessage + " was not successful.  Reason: " + invoiceResult.getErrorMessage());
+		}
+		updateFeeItemInvoiceFlag(feeSeq, finvoice);
+	}else {
+		logDebug("**ERROR: assessing fee (" + fcode + "): " + assessFeeResult.getErrorMessage());
+		feeSeq = null;
+	}
+	return feeSeq;
+}catch(err){
+	logDebug("An error occurred in addFee: " + err.message);
+	logDebug(err.stack);
+}}
+//end corrected standard functions 
 
 //INCLUDES_SEP_CUSTOM END
